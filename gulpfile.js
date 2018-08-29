3e212871ff4fcc39765a19acd88c5d2df4008e6c
Myles Braithwaite <me@mylesbraithwaite.com>
2018-08-28 20:01:07 -0400

    :lipstick: Add some images to the landing page.

--- gulpfile.js
+++ gulpfile.js
@@ -59,7 +59,7 @@ gulp.task('start', ['clean', 'build'], function() {
   });
 
   gulp.watch('src/style.scss', ['styles']);
-  gulp.watch(['**/*.rst', '**/*.tmpl'], ['nikola-build']);
+  gulp.watch(['**/*.rst', '**/*.html', '**/*.tmpl'], ['nikola-build']);
 });
 
 gulp.task('default', ['build']);
